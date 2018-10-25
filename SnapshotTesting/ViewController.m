4d5785d0b8cf6323ea0dbdc93124ac6ebc6cab41
Rami Abou Ghanem <raboughanem@google.com>
2018-10-25 09:57:28 -0400

    Initial Commit

---
+++ SnapshotTesting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SnapshotTesting
+//
+//  Created by Rami Abou Ghanem on 10/25/18.
+//  Copyright © 2018 Rami Abou Ghanem. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
